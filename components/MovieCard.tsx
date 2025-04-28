e3f1537999664ac519493fd20adc00c9094ddeb4
Gabriel Marinho <gabriel.anjo722@gmail.com>
2025-04-28 10:19:59 -0300

    feat(init): initial setup, routing & navigation, fetch movies service, custom hook useFetch, home screen UI

---
+++ components/MovieCard.tsx
@@ -0,0 +1,42 @@
+import { View, Text, TouchableOpacity, Image } from 'react-native'
+import React from 'react'
+import { Link } from 'expo-router'
+import { icons } from '@/constants/icons'
+
+const MovieCard = ({
+    id, poster_path, title, vote_average, release_date, vote_count, adult
+}: Movie ) => {
+  return (
+   <Link href={`/movies/${id}`} asChild>
+
+    <TouchableOpacity className='w-[30%]'>
+        <Image 
+            source={{
+                uri: poster_path ? `https://image.tmdb.org/t/p/w500${poster_path}` : "https://placehold.co/600x400/1a1a1a/ffffff.png"
+            }}
+            className='w-full h-52 rounded-lg'
+            resizeMode='cover'
+        />
+
+        <Text className='text-sm font-bold text-white' numberOfLines={1}>{title}</Text>
+
+        <View className='flex-row items-center justify-start gap-x-1'>
+            <Image source={icons.star} className='size-4' />
+            <Text className='text-xs text-white font-bold uppercase'>{Math.round(vote_average/2)}</Text>
+            <Text className='text-xs text-light-300 font-bold uppercase'>({Intl.NumberFormat('en', { notation: 'compact', compactDisplay: 'short' }).format(vote_count)})</Text>
+        </View>
+
+        <View className='flex-row items-center justify-between'>
+            <View className='flex-row gap-x-2'>
+                <Text className='text-xs font-medium text-light-300 uppercase'>Movie</Text>
+                {/* for the future | add movie category name  */}
+            </View>
+            <Text className='text-xs text-light-300 font-medium mt-1'>{release_date.split("-")[0]}</Text>
+        </View>
+    </TouchableOpacity>
+
+   </Link>
+  )
+}
+
+export default MovieCard
